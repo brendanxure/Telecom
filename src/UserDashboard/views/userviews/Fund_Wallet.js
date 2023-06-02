fe44c4cfb91815249b49e4f2a46895a958e7bf84
Uloma Okenyi <uloma.okenyi@senior.co.uk>
2023-06-02 02:15:16 +0100

    paystack oayment integration

--- src/UserDashboard/views/userviews/Fund_Wallet.js
+++ src/UserDashboard/views/userviews/Fund_Wallet.js
@@ -1,6 +1,5 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 // react component that copies the given text inside your clipboard
-import { CopyToClipboard } from "react-copy-to-clipboard";
 // reactstrap components
 import {
   Card,
@@ -8,54 +7,107 @@ import {
   CardBody,
   Container,
   Row,
-  Col,
-  UncontrolledTooltip,
 } from "reactstrap";
 // core components
-import Header from "../../components/Headers/Header.js";
-import { Button, Form, Input, Select } from 'antd'
+import { Button, Form, Input } from 'antd'
 import { Helmet } from 'react-helmet-async'
 import { useSelector } from "react-redux";
 import { getUser } from "../../../features/Auth/AuthSlice.jsx";
+import { baseApiUrl } from "../../../Utils/constants.js";
+import axios from 'axios';
+import { toast } from "react-toastify";
+import { json } from "react-router-dom";
 
 const Fund_Wallet = () => {
-    const {user} = useSelector(getUser)
-    return (
-      <>
-        <Helmet defer={false}>
-          <title>Telecom || Fund Wallet</title>
-        </Helmet>
-       <div className="header bg-gradient-info pb-8 pt-5 pt-md-8" style={{height : "70vh"}}></div>
-        {/* Page content */}
-        <Container className="" style={{marginTop: "-22rem"}} fluid>
-          {/* Table */}
-          <Row>
-            <div className="col">
-              <Card className="shadow">
-                <CardHeader className="bg-transparent">
-                  <h3 className="mb-0">Fund Wallet(Paystack)</h3>
-                </CardHeader>
-                <CardBody>
-                <Form className='w-100 bg-white p-4 rounded-xl '>
-            <label htmlFor="">Name</label>
-            <Form.Item>
-              <Input disabled value={user?.username}/>
-            </Form.Item>
-            <label htmlFor="">Amount</label>
-            <Form.Item>
-              <Input />
-            </Form.Item>
-            <Form.Item>
-            <Button className="bg-gradient-info text-white">Fund Wallet</Button>
-            </Form.Item>
-          </Form>
-                </CardBody>
-              </Card>
-            </div>
-          </Row>
-        </Container>
-      </>
-    );
+  const { user } = useSelector(getUser)
+  const [amount, setAmount] = useState(0)
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const reference = urlParams.get('trxref');
+    const verifyTransaction = async () => {
+      try {
+        const headers = {
+          'Authorization': `Bearer ${user?.accessToken}`,
+          'Content-Type': 'application/json'
+
+        };
+        const response = await axios.get(baseApiUrl + `/api/paystack/verify/${reference}`, { headers });
+        if (response.status === 200) {
+          toast.success(" Wallet Funded Sucessfully ")
+          window.location.href = "/admin/fundwallet";
+        }
+        else {
+          toast.error(" Wallet Not  Funded Sucessfully due to " + response.data.message)
+        }
+      }
+      catch (error) {
+        console.log(error);
+      }
+    };
+
+    if (reference) {
+      verifyTransaction();
+    }
+  }, []);
+
+
+  const handleFundWallet = async (e) => {
+    e.preventDefault();
+    const headers = {
+      'Authorization': `Bearer ${user?.accessToken}`,
+      'Content-Type': 'application/json'
+    };
+    const data = {
+      amount: amount
+    }
+
+    try {
+      const response = await axios.post(baseApiUrl + "/api/paystack/initialize", data, { headers });
+      if (response.data.status) {
+        window.location.href = response.data.data.authorization_url;
+      }
+    }
+    catch (error) {
+      console.log(error);
+    }
+  };
+
+
+  return (
+    <>
+      <Helmet defer={false}>
+        <title>Telecom || Fund Wallet</title>
+      </Helmet>
+      <div className="header bg-gradient-info pb-8 pt-5 pt-md-8" style={{ height: "70vh" }}></div>
+      {/* Page content */}
+      <Container className="" style={{ marginTop: "-22rem" }} fluid>
+        {/* Table */}
+        <Row>
+          <div className="col">
+            <Card className="shadow">
+              <CardHeader className="bg-transparent">
+                <h3 className="mb-0">Fund Wallet(Paystack)</h3>
+              </CardHeader>
+              <CardBody>
+                <Form className='w-100 bg-white p-4 rounded-xl ' >
+                  <label htmlFor="">Name</label>
+                  <Form.Item>
+                    <Input disabled value={user?.username} />
+                  </Form.Item>
+                  <label htmlFor="">Amount</label>
+                  <Form.Item>
+                    <Input value={amount} onChange={(event) => { setAmount(event.target.value) }} />
+                  </Form.Item>
+                  <Button type="button" onClick={handleFundWallet} className="bg-gradient-info text-white">Fund Wallet</Button>
+                </Form>
+              </CardBody>
+            </Card>
+          </div>
+        </Row>
+      </Container>
+    </>
+  );
 }
 
 export default Fund_Wallet

06a821a302a26edfe8d43188623c65db46f9fff5
brendanxure <brendanxure@outlook.com>
2023-06-24 10:30:05 +0100

    update

--- src/features/Wallet/WalletSlice.jsx
+++ src/features/Wallet/WalletSlice.jsx
@@ -8,15 +8,9 @@ const balance = JSON.parse(localStorage.getItem("userTelecomBalance"))
 const WalletSlice = createSlice({
   name: "wallet",
   initialState : {
-    balance: balance ? balance : 0,
+    balance: balance ? balance : 0
   },
   reducers: {
-    reset: (state) => {
-        state.isLoading = false
-        state.isError= false
-        state.isSuccess= false
-        state.message=""
-    },
     updateWalletBalance: (state, action) => {
         state.balance = action.payload;
       },
